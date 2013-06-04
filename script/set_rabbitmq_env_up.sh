ac0f1e690062aafc66f1583b90e08db2b0036eb4
Lee Henson <lee.m.henson@gmail.com>
2013-06-04 13:18:39 +0100

    undo double rmq user definition

--- script/set_rabbitmq_env_up.sh
+++ script/set_rabbitmq_env_up.sh
@@ -7,7 +7,6 @@ rabbitmqctl set_permissions -p "travisci.development" travisci_hub ".*" ".*" ".*
 rabbitmqctl set_permissions -p "travisci.development" guest      ".*" ".*" ".*"
 
 rabbitmqctl add_vhost "travisci.vagrant"
-rabbitmqctl add_user travisci_hub travisci_hub_password
 
 rabbitmqctl set_permissions -p "travisci.vagrant" travisci_hub ".*" ".*" ".*"
 rabbitmqctl set_permissions -p "travisci.vagrant" guest      ".*" ".*" ".*"
